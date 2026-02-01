6c6707b01f414fb903537f97d61cde042dcd45d7
BitCoder <alejo_2986@hotmail.com>
2026-02-01 14:59:29 -0600

    recactor(authsechema): implementar errores del grupo e errores

--- src/lib/validations/auth.schema.ts
+++ src/lib/validations/auth.schema.ts
@@ -1,15 +1,16 @@
 import { z } from 'zod';
 import { ValidationRules } from './validation-rules';
+import { ErrorMessages } from '../errors/message-errors';
 
 export const loginSchema = z.object({
     username: z
         .string()
-        .min(1, 'El usuario es requerido')
+        .min(1, ErrorMessages.LOGIN_USERNAME_EMPTY)
         .max(ValidationRules.USERNAME.MAX_LENGTH, `Máximo ${ValidationRules.USERNAME.MAX_LENGTH} caracteres`),
 
     password: z
         .string()
-        .min(1, 'La contraseña es requerida')
+        .min(1, ErrorMessages.LOGIN_PASSWORD_EMPTY)
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
