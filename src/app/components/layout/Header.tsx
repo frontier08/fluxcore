aacaf7de20aa167e00635118b9cc52484f66998d
BitCoder <alejo_2986@hotmail.com>
2026-01-31 19:54:44 -0600

    to be fixed at the top and include a navigation link to home.

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,10 +1,13 @@
 import { LogoFluxCoreLarge } from "../logos/LogoFluxCoreLarge";
 import styles from "./layout.module.scss";
+import Link from "next/link";
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <LogoFluxCoreLarge width={60} height={60} />
+            <Link href="/">
+                <LogoFluxCoreLarge width={60} height={60} />
+            </Link>
         </header>
     );
 }
