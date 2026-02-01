d9662ab40074a112f7ee53419b8c5449847775a0
BitCoder <alejo_2986@hotmail.com>
2026-02-01 12:30:01 -0600

    feat(ui): enhance authentication layout and add not-found page [UI]: Implementa una página not-found personalizada con su respectivo módulo SCSS. [Styles]: Actualiza globals.css y auth.module.scss para mejorar el diseño y la consistencia visual. [Pages]: Integra la lógica de sesión y estilos actualizados en la página de login.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,86 @@
-import { Button, Flex, Input, Divider } from "lambda-ui-components";
+"use client";
+import { Button, Input, Divider, Alert } from "lambda-ui-components";
 import styles from "../auth.module.scss";
 import { RectangleEllipsis, User2, UserCircle2 } from "lucide-react";
 import { Icon } from "@/app/components/Icon";
+import { useAuth } from "@/hooks/useAuth";
+import { LoginCredentials } from "@/typesAPI/auth.types";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/validations/auth.schema";
 
 export default function LoginPage() {
+    const { login, isLoading, error } = useAuth();
+    const { handleSubmit, control, setError, formState: { errors } } = useForm<LoginCredentials>({ resolver: zodResolver(loginSchema) });
+
+
+    const onSubmit = async (data: LoginCredentials) => {
+        console.log(data);
+        console.log(errors);
+        console.log(error);
+
+
+        await login(data);
+    };
+
+    console.log(errors);
+
+
     return (
         <div className={styles["login_page"]}>
             <div>
                 <Icon icon={UserCircle2} size={80} />
-                <h1>Bienvendido</h1>
+                <h1>Bienvenido</h1>
                 <p>Por favor, ingresa tus credenciales para iniciar sesión</p>
-                <form className={styles["login_form"]} action="">
-                    <Input placeholder="Juan Pérez" label="Usuario" prefix={<Icon icon={User2} />} />
-                    <Input placeholder="Introduce tu contraseña" type="password" prefix={<Icon icon={RectangleEllipsis} />} label="Contraseña" />
+                {(error && !isLoading) && <Alert
+                    title="Autenticación fallida"
+                    message={error!}
+                    variant="solid"
+                    color="danger"
+                    size="small"
+                />}
+                <form className={styles["login_form"]} onSubmit={handleSubmit(onSubmit)}>
+                    <Controller
+                        control={control}
+                        name="username"
+                        render={({ field }) => (
+                            <Input
+                                {...field}
+                                placeholder="Juan Pérez"
+                                label="Usuario"
+                                prefix={<Icon icon={User2} />}
+                                invalid={!!errors.username}
+                                errorMessage={errors.username?.message}
+                                required
+                            />
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field }) => (
+                            <Input
+                                {...field}
+                                placeholder="Introduce tu contraseña"
+                                type="password"
+                                prefix={<Icon icon={RectangleEllipsis} />}
+                                label="Contraseña"
+                                invalid={!!errors.password}
+                                errorMessage={errors.password?.message}
+                                required
+                            />
+                        )}
+                    />
                     <Divider spacing={20} />
-                    <Button color="primary" size="medium" block>Iniciar sesión</Button>
+                    <Button
+                        color="primary"
+                        size="medium"
+                        block
+                        loading={isLoading}
+                        disabled={isLoading}
+                        loadingText="Iniciando sesión..."
+                        label="Iniciar sesión"
+                    />
                 </form>
             </div>
         </div>
---
+++ src/app/not-found.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import styles from "./not-found.module.scss";
+import { Button, Flex } from "lambda-ui-components";
+import { useRouter } from "next/navigation";
+import { getHomeRouteForRole } from "../lib/utils/auth-utils";
+import { LogoFluxCore } from "./components/logos/LogoFluxCore";
+import { useAuth } from "@/hooks/useAuth";
+import { Icon } from "./components/Icon";
+import { CloudOff } from "lucide-react";
+
+export default function NotFound() {
+    const { user } = useAuth();
+    const router = useRouter();
+    const homeRoute = getHomeRouteForRole(user?.role || 'CASHIER');
+    console.log(user);
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <LogoFluxCore width={100} height={100} />
+                <Flex gap={10} direction="column" align="center">
+                    <h1>404</h1>
+                    <Icon icon={CloudOff} size={50} />
+                </Flex>
+                <h2>Página no encontrada</h2>
+                <p>Parece que la ruta que buscas no existe o fue movida.
+                    Verifica la URL o usa los botones de abajo para volver a un lugar seguro.
+                </p>
+                <Flex gap={10}>
+                    <Button
+                        color="primary"
+                        size="medium"
+                        block
+                        onClick={() => router.push(homeRoute)}
+                    >
+                        Volver a {user?.role === 'CASHIER' ? 'Ventas' : 'mi Panel'}
+                    </Button>
+                    <Button
+                        color="primary"
+                        size="medium"
+                        block
+                        variant="outline"
+                        onClick={() => router.push('/')}
+                    >
+                        Ir a inicio
+                    </Button>
+                </Flex>
+            </div>
+        </div>
+    );
+}
